a84d45d713d5be3e18a584981c0376a5b9a75ddd
Cbcasper <T84GqsZj>
2022-11-26 14:47:19 +0100

    created reddit file

--- src/main.py
+++ src/main.py
@@ -12,14 +12,6 @@ import praw
 
 load_dotenv()
 
-reddit = praw.Reddit(
-    client_id=os.getenv("CLIENT_ID"),
-    client_secret=os.getenv("CLIENT_SECRET"),
-    user_agent="aita analyser",
-    username=os.getenv("username"),
-    password=os.getenv("password")
-)
-
 data = pandas.read_csv("aita_clean.csv")
 
 
---
+++ src/reddit.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+import os
+import praw
+
+load_dotenv()
+
+reddit = praw.Reddit(
+    client_id=os.getenv("CLIENT_ID"),
+    client_secret=os.getenv("CLIENT_SECRET"),
+    user_agent="aita analyser",
+    username=os.getenv("username"),
+    password=os.getenv("password")
+)
+
+for submission in reddit.subreddit("AmITheAsshole").hot(limit=10):
+    print(submission.title)

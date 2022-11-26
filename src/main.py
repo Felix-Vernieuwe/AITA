58514ef34f0eb35698c7fda4bc9404157df885e9
Fevol <vernieuwefelix@gmail.com>
2022-11-26 14:00:49 +0100

    attempt at indexing csv

--- src/main.py
+++ src/main.py
@@ -1,14 +1,38 @@
 import pandas
+import pandas as pd
+import whoosh.fields
+
+from schema import PostSchema
+from whoosh import index
+import tqdm
+import datetime
 
 data = pandas.read_csv("aita_clean.csv")
+
+# Process data and put into Lucene
+ix = index.create_in('indexdir', PostSchema)
+
+writer = ix.writer()
+
+# Set row timestamp as int type
+data['timestamp'] = pd.to_datetime(data['timestamp'], unit="s")
+
+loading_bar = tqdm.tqdm(len(data))
 for index, row in data.iterrows():
-    print("*" * 100)
-    print("ID")
-    print(row["id"])
-    print("TITLE")
-    print(row["title"])
-    print("BODY")
-    print(row["body"])
-    print()
-    if index == 20:
-        break
+    writer.add_document(url=row["id"], timestamp=row["timestamp"], title=row["title"], body=row["body"], edited=row["edited"],
+                        verdict=row["verdict"], score=row["score"], num_comments=row["num_comments"])
+    loading_bar.update(1)
+writer.commit()
+
+# data = pandas.read_csv("aita_clean.csv")
+# for index, row in data.iterrows():
+#     print("*" * 100)
+#     print("ID")
+#     print(row["id"])
+#     print("TITLE")
+#     print(row["title"])
+#     print("BODY")
+#     print(row["body"])
+#     print()
+#     if index == 20:
+#         break

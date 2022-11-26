d94a119cd84239375ba7f01847bff9314bd95e1d
JoshiDhima <joshikaposhi@hotmail.com>
2022-11-26 19:48:28 +0100

    print search results & url

--- praw_credentials.py
+++
@@ -1,9 +0,0 @@
-import praw
-
-reddit = praw.Reddit(
-    client_id="VE2xMSVoXBnC2OIeN9_6WA",
-    client_secret="jLWthBL55QRRR_al0YNl81gyD2PiSw",
-    user_agent="aita analyser",
-    username="cbcasper",
-    password="beer@123321"
-)

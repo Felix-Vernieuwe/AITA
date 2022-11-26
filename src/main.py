cd76b0136a223e2ed930bf7b91fa97570a5dd34f
JoshiDhima <joshikaposhi@hotmail.com>
2022-11-26 14:35:46 +0100

    set up env variables for reddit app

--- src/main.py
+++ src/main.py
@@ -2,11 +2,23 @@ import pandas
 import pandas as pd
 import whoosh.fields
 
+from dotenv import load_dotenv
 from schema import PostSchema
 from whoosh import index
 import tqdm
 import datetime
 import os
+import praw
+
+load_dotenv()
+
+reddit = praw.Reddit(
+    client_id=os.getenv("CLIENT_ID"),
+    client_secret=os.getenv("CLIENT_SECRET"),
+    user_agent="aita analyser",
+    username=os.getenv("username"),
+    password=os.getenv("password")
+)
 
 data = pandas.read_csv("aita_clean.csv")
 
